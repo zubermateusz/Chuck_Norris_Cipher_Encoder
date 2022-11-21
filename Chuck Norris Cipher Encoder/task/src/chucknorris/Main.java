e11e4fbe52dcc8229d0da1a8e14a2bc9e1ae796e
Mateusz <zubermateusz@gmail.com>
2022-11-21 21:48:11 +0100

    Stage 1/5 wprowadzono pobranie tekstu od uzytkownika i wypisanie ze spacjami po kazdym znaku Stage 2/5 wprowadzono zmiane kazdego znaku na binnary i wypisanie w oddzielnych liniach

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -1,7 +1,35 @@
 package chucknorris;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Input string:");
+        // wczytanie tekstu od uzytkownika
+        StringBuilder text = new StringBuilder(new Scanner(System.in).nextLine());
+        StringBuilder textOutput = new StringBuilder();
+        // sprawdzenie czy cos zostalo wpisane
+        //if (text.length() > 0) { //usuniete dla Stage 2/5
+            //wyswietlenie tekstu w formie binarnej STAGE 2/5
+            //System.out.println(" ");
+
+            textOutput.append("The result:\n");
+            for (int i = 0; i < text.length(); i++) {
+                // pobiera char, rzutuje na int, przerabia na binary i parsuje na int, dodaje zera przez liczba do 7 cyfr
+                textOutput.append(text.charAt(i) + " = " + String.format("%07d", Integer.parseInt(Integer.toBinaryString(text.charAt(i)))) + "\n");
+            }
+/*            // wstawienie spacji po kazdym znaku STAGE 1/5
+            for (int i = 0; i < text.length(); i++) {
+                textOutput.append(text.charAt(i)).append(' ');
+            }
+            // usuniecie spacji na koncu napisu
+            textOutput.deleteCharAt(textOutput.length() - 1);
+            // wyswietlenie tekstu
+            //System.out.println(textOutput);
+
+ */
+        //}
+        System.out.println(textOutput);
     }
 }

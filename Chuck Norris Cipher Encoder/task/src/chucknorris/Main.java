4026923716183b410fc60322b44268cd660b44f9
Mateusz <zubermateusz@gmail.com>
2022-11-22 11:46:35 +0100

    dodano metode changeTextToBinary, ktora zmienia tekst zwykly na binarny, dodano metode printResult, ktora wyswietla tekst do consoli w formacie The result:

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -7,18 +7,14 @@ public class Main {
 
         System.out.println("Input string:");
         // wczytanie tekstu od uzytkownika
-        StringBuilder text = new StringBuilder(new Scanner(System.in).nextLine());
-        StringBuilder textOutput = new StringBuilder();
+        StringBuilder mainText = new StringBuilder(new Scanner(System.in).nextLine());
+        StringBuilder tempText = new StringBuilder();
         // sprawdzenie czy cos zostalo wpisane
         //if (text.length() > 0) { //usuniete dla Stage 2/5
             //wyswietlenie tekstu w formie binarnej STAGE 2/5
             //System.out.println(" ");
-
-            textOutput.append("The result:\n");
-            for (int i = 0; i < text.length(); i++) {
-                // pobiera char, rzutuje na int, przerabia na binary i parsuje na int, dodaje zera przez liczba do 7 cyfr
-                textOutput.append(text.charAt(i) + " = " + String.format("%07d", Integer.parseInt(Integer.toBinaryString(text.charAt(i)))) + "\n");
-            }
+        tempText = changeTextTToBinary(mainText); // przerabia tekst zwykly na binarny
+        printResult(tempText); // wyswietla tekst zaszyfrowany
 /*            // wstawienie spacji po kazdym znaku STAGE 1/5
             for (int i = 0; i < text.length(); i++) {
                 textOutput.append(text.charAt(i)).append(' ');
@@ -30,6 +26,21 @@ public class Main {
 
  */
         //}
-        System.out.println(textOutput);
+
+    }
+
+    private static StringBuilder changeTextTToBinary(StringBuilder text) {
+        StringBuilder tempText = new StringBuilder(); // binary text
+        for (int i = 0; i < text.length(); i++) {
+            // pobiera char, rzutuje na int, przerabia na binary i parsuje na int, dodaje zera przez liczba do 7 cyfr
+            tempText.append(String.format("%07d",Integer.parseInt(Integer.toBinaryString(text.charAt(i)))));
+        }
+        return tempText;
+    }
+
+    private static void printResult(StringBuilder resultText) {
+        StringBuilder textOutput = new StringBuilder();
+        textOutput.append("The result:\n");
+        textOutput.append(resultText);
     }
 }
